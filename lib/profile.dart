f96c414c9cccb8684a759024bb03e5af2cfced6c
Dzaka29 <aniszaka29@gmail.com>
2025-10-23 13:52:27 +0700

    Belajar Padding SizeBox Container

--- lib/profile.dart
+++ lib/profile.dart
@@ -17,19 +17,48 @@ class ProfileCard extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Bambang Slebew',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.amberAccent,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.redAccent,
+                    spreadRadius: 5.0,
+                    blurRadius: 7.0,
+                    offset: Offset(0, 3),
+                  )
+                ]
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20, bottom: 50),
+                    child: Text(
+                      'Name',
+                       style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Bambang Slebew',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
             ),
-            Row(
-              children: <Widget>[
-                Column(
-                  children: <Widget>[],
-                )
-                Column(
-                  children: <Widget>[],
-                )
-              ],
+            Column(
+              children: <Widget>[],
+            ),
+            Column(
+              children: <Widget>[],
             )
           ],
         ),
